a81f7e935ec060ddc2e11799508d37f04a4ecfa3
Fariz Izham <farizizham01@gmail.com>
2025-09-11 03:45:50 +0800

    fixed journal iam data secretsmanager key

--- projects/journal/env/dev/iam/data.tf
+++ projects/journal/env/dev/iam/data.tf
@@ -1,9 +1,9 @@
 data "terraform_remote_state" "secretsmanager" {
   backend = "s3"
   config = {
-    bucket = "dev-tf-state-bucket-matchbox3361"
-    key    = "dev/secretsmanager/terraform.tfstate"
-    region = "ap-southeast-1"
+    bucket         = "dev-tf-state-bucket-matchbox3361"
+    key            = "dev/journal/secretsmanager/terraform.tfstate"
+    region         = "ap-southeast-1"
   }
 }
 
